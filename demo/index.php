a8c721a5029a9aa48bbd86fd6a1d543e79df57d1
W_wang <1352255400@qq.com>
2018-06-07 15:21:56 +0800

    code

--- demo/index.php
+++ demo/index.php
@@ -9,16 +9,20 @@
 ?>
 
 <style type="text/css">
-	.code_div{width:350px;background:#ccc;margin:0 auto;margin-top:200px;padding:10px}
+	.code_div{width:350px;background:#ccc;margin:0 auto;margin-top:150px;padding:10px}
+	.code_div h1{color:#28A845;text-align:center;margin:0}
+	.code_div a{width:350px;height:50px;line-height:50px;text-decoration:none;background:#333;color:#fff;font-weight:bold;cursor:pointer;display:block;text-align:center;margin-top:10px}
+	.code_div img{margin-top: 10px;}
 	.code_div input{width:350px;height:50px;border:1px solid #28A845;margin-top:10px}
-	.code_div input.btn{background:#28A845;color:#fff;font-weight:bold}
+	.code_div input.btn{background:#28A845;color:#fff;font-weight:bold;cursor: pointer;}
 	.return_info{width:350px;line-height:30px;background:#ccc;margin:0 auto;padding:10px;padding-top:0;text-align:center;display:none;font-weight:bold}
 </style>
 <div class="code_div">
-	<p>composer require 1352255400/phpgifcode</p>
+	<h1>php动态验证码类</h1>
 	<img class='code_img fr' src="getCode.php" onclick= this.src="getCode.php?"+Math.random() />
 	<input type="text" name="code" class="code" value="<?=$code?>" placeholder="请输入验证码">
 	<input type="button" class="btn" value="验 证">
+	<a href="https://github.com/1352255400/code" target="_blank" title="composer require 1352255400/phpgifcode">获取源代码[GITHUB]</a>
 </div>
 <div class="return_info"></div>
 <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
